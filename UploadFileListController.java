cc984b4c64ba0e11b471ac55e666e5b57bc5eac5
studaw <92657880+studaw@users.noreply.github.com>
2022-05-19 18:10:42 +0800

    Add files via upload

---
+++ UploadFileListController.java
@@ -0,0 +1,21 @@
+package com.blog.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author blog
+ * @since 2022-04-12
+ */
+@RestController
+@RequestMapping("/upload-file-list")
+public class UploadFileListController {
+
+}
+
